bf4e0d2bccf18320ccf939c500f8783daa4fee22
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-10-12 12:44:00 +0100

    Quicksave

---
+++ Labsheet 3/Q1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age: ");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+
+            if (ValidateAge(age))
+            {
+                Console.WriteLine("You can now access the site");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you cannot access this site");
+            }
+        }
+
+        static bool ValidateAge(int age)
+        {
+            bool validAge = false;
+
+            if (age >= 18 && age <= 21)
+            {
+                validAge = true;
+            }
+            else
+            {
+                return validAge;
+            }
+        }
+    }
+}
