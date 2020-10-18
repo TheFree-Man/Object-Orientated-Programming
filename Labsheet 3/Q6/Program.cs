6e05b363c858e201a846e44bfd2c8d6842577afd
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-10-18 22:25:23 +0100

    Quicksave

---
+++ Labsheet 3/Q6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] fileReviews = new int[,]
+            {
+                { 3,4,5 },
+                { 1,2,1 },
+                { 5,4,2 }
+            };
+
+            double[] averages = new double[3];
+
+            int total;
+
+            for (int i = 0; i < fileReviews.GetLength(0); i++)
+            {
+                total = 0;  
+
+                for (int j = 0; j < fileReviews.GetLength(1); j++)
+                {
+                    total = fileReviews[i, j];
+                }
+
+                averages[i] = (double)total / 3;
+            }
+
+            foreach (double average in averages)
+            {
+                Console.WriteLine("Average is : {0:F2}", average);
+            }
+        }
+    }
+}
---
+++ Labsheet 3/Q7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> team = new List<string>();
+
+            Console.WriteLine("Enter team member name (-1 to quit): ");
+            string input = Console.ReadLine();
+
+            while (input != "-1")
+            {
+                team.Add(input);
+
+                Console.WriteLine("Enter team member name, -1 to quit: ");
+                input = Console.ReadLine();
+            }
+
+            foreach (string member in team)
+            {
+                Console.WriteLine(member);
+            }
+
+            team.Sort();
+
+            foreach (string member in team)
+            {
+                Console.WriteLine(member);
+            }
+        }
+    }
+}
