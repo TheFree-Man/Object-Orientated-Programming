3af0ad90b8be6bb6084284c803324ac7eaed0830
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-10-18 16:56:25 +0100

    Quicksave

--- Labsheet 3/Q5/Program.cs
+++ Labsheet 3/Q5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 
 namespace Q5
 {
@@ -6,7 +7,31 @@ namespace Q5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int stock = 10, sales = 5;
+
+            Console.WriteLine("Stock on hand = {0}", stock);
+
+            if (UpdateStock(ref stock, sales))
+            {
+                Console.WriteLine("Sale successful, sold {0}, remaining stock {1}", sales, stock);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient stock");
+            }
+        }
+
+        static bool UpdateStock(ref int stock, int sales)
+        {
+            bool stockUpdated = false;
+
+            if(stock >= sales)
+            {
+                stock -= sales;
+                stockUpdated = true;
+            }
+
+            return stockUpdated;
         }
     }
 }
