53c5430c3485123f00278dc15319ec7205534720
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-10-18 16:42:58 +0100

    Quicksave

--- Labsheet 3/Q3/Program.cs
+++ Labsheet 3/Q3/Program.cs
@@ -6,7 +6,29 @@ namespace Q3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] {3, 5, 7, 9, 11 };
+
+            DisplayArray(numbers);
+
+            Zero(numbers);
+
+            DisplayArray(numbers);
+        }
+
+        static void DisplayArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Element {0} : {1}", i, numbers[i]);
+            }
+        }
+
+        static void Zero(int[] numbers)
+        {
+            for (int i = 0; i< numbers.Length; i++)
+            {
+                numbers[i] = 0;
+            }
         }
     }
 }
---
+++ Labsheet 3/Q4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(2,10));
+        }
+
+        static int Sum(int number1, int number2)
+        {
+            int total = 0, temp;
+
+            if (number2 < number1)
+            {
+                temp = number2;
+                number2 = number1;
+                number1 = temp;
+            }
+
+            for (int i = number1; i <=number2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    total += i;
+                }
+            }
+
+            return total;
+        }
+    }
+}
