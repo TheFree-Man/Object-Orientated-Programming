edd09407267db5baf1b29d3b80d0560a136a5310
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-11-16 14:24:26 +0000

    Quicksave

---
+++ CA 1/Q2/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Card
+    {
+        public string Rank { get; set; }
+        public string suit { get; set; }
+    }
+}
---
+++ CA 1/Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer sndPlayer = new SoundPlayer();
+            sndPlayer.SoundLocation = @"C:\Users\Uri\Desktop\Music\Music for game\sp8.wav";
+            sndPlayer.PlayLooping();
+
+            List<Card> deck = CreateCards();
+        }
+
+        private static List<Card> CreateCards()
+        {
+            string[] suits = { "Diamonds", "Spades", "Clubs", "Hearts" };
+            string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        }
+    }
+}
