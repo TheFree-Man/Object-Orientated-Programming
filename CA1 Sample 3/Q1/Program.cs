0517f9e64fa186eb811f6037e3542836045e2680
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-11-29 20:32:14 +0000

    Quicksave

--- CA1 Sample 3/Q1/Program.cs
+++ CA1 Sample 3/Q1/Program.cs
@@ -10,6 +10,38 @@ namespace Q1
     {
         static void Main(string[] args)
         {
+            Song s1 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
+            Song s2 = new Song("DANCE MMONKEY", "TONES & I", 4.20, Genre.Dance);
+            Song s3 = new Song("CIRCLES", "POST MALONE", 3.15, Genre.Pop);
+            Song s4 = new Song("RIDE IT", "REGARD", 3.37, Genre.Dance);
+            Song s5 = new Song("SOUTH OF THE BORDER", "ED SHEERAN", 4.26, Genre.Pop);
+
+            List<Song> playlist = new List<Song>();
+
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+
+            //foreach (Song song in playlist)
+            //{
+            //    Console.WriteLine(song);
+            //}
+
+            Display(playlist);
+
+            Console.ReadLine();
+        }
+
+        private static void Display(List<Song> playlist)
+        {
+            Console.WriteLine("{0,-20}{1,-25}{2,-10}{3,-10}", "Artist", "Song", "Duration", "Genre");
+
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine($"{song.Artist, -20}{song.Title, -25}{song.Duration, -10}{song.MusicGenre, -10}");
+            }
         }
     }
 }
---
+++ CA1 Sample 3/Q1/Song.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        
+        public Genre MusicGenre { get; set; }
+
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other) { }
+
+        public Song() : this("unknown", "unknown") { }
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
+    }
+
+    public enum Genre { Rock, Pop, Dance, Other}
+}
