4f1ea0d7b3d59f0e82f6f84ef56d81da677a513f
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-12-18 15:40:51 +0000

    Quicksave

--- CA 2/Q1/Employee.cs
+++ CA 2/Q1/Employee.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Name:            Mark Gilmartin
+ * 
+ * Description:     WPF application that lists out employees and lets you view, add, delete, and update the employees
+ * 
+ * Date:            8/12/2020
+ * 
+ * Last Modified:   18/12/2020
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,59 +18,59 @@ namespace Q1
 {
     public abstract class Employee
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; }       // Sets attributes for employee's first name
+        public string LastName { get; set; }        // Sets attributes for employee's surname
 
-        public Employee(string firstName, string lastName)
+        public Employee(string firstName, string lastName)      // Sets attributes as strings
         {
             FirstName = firstName;
             LastName = lastName;
         }
 
-        public abstract decimal CalculateMonthlyPay();
+        public abstract decimal CalculateMonthlyPay();      // Main CalCulateMonthlyPay() method 
     }
 
     public class FullTimeEmployee : Employee
     {
-        public decimal Salary { get; set; }
+        public decimal Salary { get; set; }     // Sets attributes for full time employee's salary
 
-        public override decimal CalculateMonthlyPay()
+        public override decimal CalculateMonthlyPay()       // Calculates monthly pay for part time employees
         {
             decimal pay = Salary / 12;
             return pay;
         }
 
-        public FullTimeEmployee(string firstName, string lastName, decimal salary) : base(firstName, lastName)
+        public FullTimeEmployee(string firstName, string lastName, decimal salary) : base(firstName, lastName)      // Sets attributes as string
         {
             Salary = salary;
         }
 
         public override string ToString()
         {
-            return string.Format($"{LastName.ToUpper()}, {FirstName} - Full Time");
+            return string.Format($"{LastName.ToUpper()}, {FirstName} - Full Time");     // Sets the formatting for how each employee will be displayed in the listbox
         }
     }
 
     public class PartTimeEmployee : Employee
     {
-        public decimal HourlyRate { get; set; }
-        public double HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }     // Sets attributes for part time employee's hourly rate
+        public double HoursWorked { get; set; }     // Sets attributes for part time employee's hurs worked
 
-        public override decimal CalculateMonthlyPay()
+        public override decimal CalculateMonthlyPay()       // Calculates monthly pay for part time employees
         {
             decimal pay = HourlyRate * (decimal)HoursWorked;
             return pay;
         }
 
-        public PartTimeEmployee(string firstName, string lastName, decimal HourlyRate, double HoursWorked) : base(firstName, lastName)
+        public PartTimeEmployee(string firstName, string lastName, decimal hourlyRate, double hoursWorked) : base(firstName, lastName)      // Sets attributes as strings
         {
-            decimal hourlyRate = HourlyRate;
-            double hoursWorked = HoursWorked;
+            HourlyRate = hourlyRate;
+            HoursWorked = hoursWorked;
         }
 
         public override string ToString()
         {
-            return string.Format($"{LastName.ToUpper()}, {FirstName} - Part Time");
+            return string.Format($"{LastName.ToUpper()}, {FirstName} - Part Time");     // Sets the formatting for how each employee will be displayed in the listbox
         }
     }
 }
--- CA 2/Q1/MainWindow.xaml.cs
+++ CA 2/Q1/MainWindow.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Name:            Mark Gilmartin
+ * 
+ * Description:     WPF application that lists out employees and lets you view, add, delete, and update the employees
+ * 
+ * Date:            8/12/2020
+ * 
+ * Last Modified:   18/12/2020
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -21,8 +31,8 @@ namespace Q1
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<Employee> employee = new ObservableCollection<Employee>();
-        ObservableCollection<Employee> filterEmployee = new ObservableCollection<Employee>();
+        ObservableCollection<Employee> employee = new ObservableCollection<Employee>();     // Creates Observible collection named "employee"
+        ObservableCollection<Employee> filterEmployee = new ObservableCollection<Employee>();       // Creates observible collection named "filterEmployee" for Filter() method
         
 
         public MainWindow()
@@ -32,11 +42,13 @@ namespace Q1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // Pre-registered employees
             FullTimeEmployee FTE1 = new FullTimeEmployee("Joe", "Murphy", 30000);
             FullTimeEmployee FTE2 = new FullTimeEmployee("Jess", "Walsh", 60000);
             PartTimeEmployee PTE1 = new PartTimeEmployee("Jane", "Jones", 9, 40);
             PartTimeEmployee PTE2 = new PartTimeEmployee("John", "Smith", 10, 38);
 
+            // Adds pre-registered employees to the listbox
             employee.Add(FTE1);
             employee.Add(FTE2);
             employee.Add(PTE1);
@@ -47,20 +59,20 @@ namespace Q1
 
         }
 
-        private void Filter()
+        private void Filter()       // Filters employees based on what checkbox is checked
         {
             filterEmployee.Clear();
-            lstbxEmployees.ItemsSource = null;
+            lstbxEmployees.ItemsSource = null;      // Clears listbox
 
-            if (chkbxFullTime.IsChecked == true && chkbxPartTime.IsChecked == true)
+            if (chkbxFullTime.IsChecked == true && chkbxPartTime.IsChecked == true)     // Checks if "Full Time" & "Part time" checkboxes are checked
             {
                 foreach (Employee employees in employee)
                 {
                     filterEmployee.Add(employees);
                 }
-                lstbxEmployees.ItemsSource = employee;
+                lstbxEmployees.ItemsSource = filterEmployee;
             }
-            else if (chkbxFullTime.IsChecked == true)
+            else if (chkbxFullTime.IsChecked == true)       // Checks if only "Full Time" checkbox is checked
             {
                 foreach (Employee employees in employee)
                 {
@@ -71,7 +83,7 @@ namespace Q1
                 }
                 lstbxEmployees.ItemsSource = filterEmployee;
             }
-            else
+            else       // Checks if only "Part Time" checkbox is checked
             {
                 foreach (Employee employees in employee)
                 {
@@ -86,16 +98,18 @@ namespace Q1
             Sorting();
         }
 
-        private void Sorting()
+        private void Sorting()      // Sorts employees alphabetically
         {
-            if (chkbxFullTime.IsChecked == true || chkbxPartTime.IsChecked == true)
+            if (chkbxFullTime.IsChecked == true || chkbxPartTime.IsChecked == true)     // Checks if employees were filered or not [see Filter() method]
             {
+                // Clears old listbox and creates new observible collection which is sorted alphabetically, then added to listbox
                 lstbxEmployees.ItemsSource = null;
                 filterEmployee = new ObservableCollection<Employee>(filterEmployee.OrderBy(i => i.LastName));
                 lstbxEmployees.ItemsSource = filterEmployee;
             }
             else
             {
+                // Clears old listbox and creates new observible collection which is sorted alphabetically, then added to listbox
                 lstbxEmployees.ItemsSource = null;
                 employee = new ObservableCollection<Employee>(employee.OrderBy(i => i.LastName));
                 lstbxEmployees.ItemsSource = employee;
@@ -109,6 +123,7 @@ namespace Q1
 
         private void lstbxEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Clears any previous selection
             tbxSalary.Clear();
             tbxHoursWorked.Clear();
             tbxHourlyRate.Clear();
@@ -116,7 +131,7 @@ namespace Q1
 
             foreach (Employee employees in employee)
             {
-                if (employees is FullTimeEmployee)
+                if (employees is FullTimeEmployee)      // Checks if employee is full time
                 {
                     FullTimeEmployee selectedEmployeeFullTime = lstbxEmployees.SelectedItem as FullTimeEmployee;
                     if (selectedEmployeeFullTime == null)
@@ -124,13 +139,14 @@ namespace Q1
                         continue;
                     }
 
+                    // Displays employee info in textboxes
                     tbxFirstName.Text = selectedEmployeeFullTime.FirstName;
                     tbxSurname.Text = selectedEmployeeFullTime.LastName;
                     rbtnFT.IsChecked = true;
                     tbxSalary.Text = Convert.ToString(selectedEmployeeFullTime.Salary);
                     tblkMonthlyPay.Text = Convert.ToString(selectedEmployeeFullTime.CalculateMonthlyPay());
                 }
-                else if (employees is PartTimeEmployee)
+                else if (employees is PartTimeEmployee)     //Checks if employee is part time
                 {
                     PartTimeEmployee selectedEmployeePartTime = lstbxEmployees.SelectedItem as PartTimeEmployee;
                     if (selectedEmployeePartTime == null)
@@ -138,23 +154,25 @@ namespace Q1
                         continue;
                     }
 
+                    // Displays employee info in textboxes
                     tbxFirstName.Text = selectedEmployeePartTime.FirstName;
                     tbxSurname.Text = selectedEmployeePartTime.LastName;
                     rbtnPT.IsChecked = true;
                     tbxHoursWorked.Text = Convert.ToString(selectedEmployeePartTime.HoursWorked);
                     tbxHourlyRate.Text = Convert.ToString(selectedEmployeePartTime.HourlyRate);
+                    tblkMonthlyPay.Text = Convert.ToString(selectedEmployeePartTime.CalculateMonthlyPay());
                 }
             } 
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        private void btnDelete_Click(object sender, RoutedEventArgs e)      // Deletes an employee from the listbox
         {
              Employee employeeForRemoval = lstbxEmployees.SelectedItem as Employee;
             employee.Remove(employeeForRemoval);
             
         }
 
-        private void btnClear_Click(object sender, RoutedEventArgs e)
+        private void btnClear_Click(object sender, RoutedEventArgs e)       // Clears all textboxes
         {
             tbxFirstName.Clear();
             tbxSurname.Clear();
@@ -166,33 +184,33 @@ namespace Q1
             tblkMonthlyPay.Text = "";
         }
 
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, RoutedEventArgs e)     // Adds a new employee based on info inputted into the textboxes
         {
             string firstName = tbxFirstName.Text;
             string surname = tbxSurname.Text;
 
-            if (rbtnFT.IsChecked == true && tbxSalary != null)
+            if (rbtnFT.IsChecked == true && tbxSalary != null)      // Determines if employee is full time by the "FT" radio button being checked
             {
                 decimal salary = decimal.Parse(tbxSalary.Text);
-                FullTimeEmployee FTE = new FullTimeEmployee(firstName, surname, salary);
+                FullTimeEmployee FTE = new FullTimeEmployee(firstName, surname, salary);        // Creates new employee
                 employee.Add(FTE);
                 Filter();
             }
-            else if (rbtnPT.IsChecked == true && tbxSalary == null)
+            else if (rbtnPT.IsChecked == true && tbxSalary.Text == "")      // Determines if employee is part time by the "PT" radio button being checked
             {
-                decimal hoursWorked = decimal.Parse(tbxHoursWorked.Text);
-                double hourlyRate = double.Parse(tbxHourlyRate.Text);
-                PartTimeEmployee PTE = new PartTimeEmployee(firstName, surname, hoursWorked, hourlyRate);
+                decimal hWorked = decimal.Parse(tbxHoursWorked.Text);
+                double hRate = double.Parse(tbxHourlyRate.Text);
+                PartTimeEmployee PTE = new PartTimeEmployee(firstName, surname, hWorked, hRate);        // Creates new employee
                 employee.Add(PTE);
                 Filter();
             }
-            else
+            else       // Shows error is something is wrong
             {
                 MessageBox.Show("Error, please try again");
             }
         }
 
-        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)      // Updates employee selected in the listbox based on what is in the textboxes
         {
             Employee employeeUpdated = lstbxEmployees.SelectedItem as Employee;
             if (employeeUpdated != null)
@@ -200,22 +218,24 @@ namespace Q1
                 string firstName = tbxFirstName.Text;
                 string surname = tbxSurname.Text;
 
-                if (rbtnFT.IsChecked == true && tbxSalary != null)
+                if (rbtnFT.IsChecked == true && tbxSalary != null)      // Determines if employee is full time by the "FT" radio button being checked
                 {
                     decimal salary = decimal.Parse(tbxSalary.Text);
-                    FullTimeEmployee FTE = new FullTimeEmployee(firstName, surname, salary);
+                    FullTimeEmployee FTE = new FullTimeEmployee(firstName, surname, salary);        // Creates new employee entry with updated information
+                    employee.Remove(employeeUpdated);       // Removes old employee entry with outdated indformation
                     employee.Add(FTE);
                     Filter();
                 }
-                else if (rbtnPT.IsChecked == true && tbxSalary == null)
+                else if (rbtnPT.IsChecked == true && tbxSalary.Text == "")      // Determines if employee is part time by the "PT" radio button being checked
                 {
                     decimal hoursWorked = decimal.Parse(tbxHoursWorked.Text);
                     double hourlyRate = double.Parse(tbxHourlyRate.Text);
-                    PartTimeEmployee PTE = new PartTimeEmployee(firstName, surname, hoursWorked, hourlyRate);
+                    PartTimeEmployee PTE = new PartTimeEmployee(firstName, surname, hoursWorked, hourlyRate);       // Creates new employee entry with updated information
+                    employee.Remove(employeeUpdated);       // Removes old employee entry with outdated indformation
                     employee.Add(PTE);
                     Filter();
                 }
-                else
+                else       // Shows error is something is wrong
                 {
                     MessageBox.Show("Error, please try again");
                 }
